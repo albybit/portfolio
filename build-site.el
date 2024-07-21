5b206c52e79e8688a31a5f2a46427102b948af10
albybit <albertocanta31@outlook.it>
2024-07-21 22:10:02 +0200

    solarized.css in local

--- build-site.el
+++ build-site.el
@@ -20,7 +20,7 @@
 (setq org-html-validation-link nil            ;; Don't show validation link
       org-html-head-include-scripts nil       ;; Use our own scripts
       org-html-head-include-default-style nil;; Use our own styles
-      org-html-head "<link rel=\"stylesheet\" href=\"http://thomasf.github.io/solarized-css/solarized-dark.min.css\" />")
+      org-html-head "<link rel=\"stylesheet\" type=\"text/css\" href=\"solarized.css\" />")
 
 ;; Define the publishing project
 (setq org-publish-project-alist
@@ -34,7 +34,17 @@
              :with-creator t            ;; Include Emacs and Org versions in footer
              :with-toc nil                ;; Include a table of contents
              :section-numbers nil       ;; Don't include section numbers
-             :time-stamp-file nil)))    ;; Don't include time stamp in file
+             :time-stamp-file nil)
+       
+      (list "org-static"
+ :base-directory "./content"
+ :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf"
+ :publishing-directory "./public"
+ :recursive t
+ :publishing-function 'org-publish-attachment
+ ))
+
+      )    ;; Don't include time stamp in file
 
 ;; Generate the site output
 (org-publish-all t)
