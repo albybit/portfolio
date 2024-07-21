646e1ac10f24ae31690223c257f0cbb142481230
albybit <albertocanta31@outlook.it>
2024-07-21 20:26:16 +0200

    solarized dark theme

--- build-site.el
+++ build-site.el
@@ -19,8 +19,8 @@
 ;; Customize the HTML output
 (setq org-html-validation-link nil            ;; Don't show validation link
       org-html-head-include-scripts nil       ;; Use our own scripts
-      org-html-head-include-default-style nil ;; Use our own styles
-      org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" />")
+      org-html-head-include-default-style nil;; Use our own styles
+      org-html-head "<link rel=\"stylesheet\" href=\"http://thomasf.github.io/solarized-css/solarized-dark.min.css\" />")
 
 ;; Define the publishing project
 (setq org-publish-project-alist
